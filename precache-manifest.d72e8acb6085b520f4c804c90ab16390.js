4a3b1d18c6b279283c9858a1c7dd94b86ef94529
Ronnasayd Machado <ronnasayd@hotmail.com>
2020-04-24 17:01:41 -0300

    Updates

---
+++ precache-manifest.d72e8acb6085b520f4c804c90ab16390.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44150c0509a872851bd4013f357e6cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54088aa66b446ce36280",
+    "url": "/static/js/2.d44b2d67.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d44b2d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d72eab99d7e116c9ad44",
+    "url": "/static/js/main.8fbae8d6.chunk.js"
+  },
+  {
+    "revision": "0d86d67642a87b908e07",
+    "url": "/static/js/runtime-main.c3b2075f.js"
+  },
+  {
+    "revision": "92f7d9ae85309fb33e49715618314a7c",
+    "url": "/static/media/description.92f7d9ae.svg"
+  },
+  {
+    "revision": "12ff661dd7332b6e6d31d4f285e40ab8",
+    "url": "/static/media/location.12ff661d.svg"
+  },
+  {
+    "revision": "a40dd72d0f00b18f6e5bfad1d18b539a",
+    "url": "/static/media/logo-default.a40dd72d.png"
+  }
+]);
